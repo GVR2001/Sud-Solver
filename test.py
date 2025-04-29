2f2ddaf41c38acb12ecf726bfc38bdd1d03af231
GVR2001 <gurveernirwal@gmail.com>
2025-04-29 14:13:27 +0100

    Created a test file
    I have added a testing suite for my code, so I can experiment and test the code I have written.

---
+++ test.py
@@ -0,0 +1,12 @@
+from utils import *
+# Solving grid
+pic1 = "53..7.... 6..195... .98....6. 8...6...3 4..8.3..1 7...2...6 .6....28. ...419..5 ....8..79"
+grid1 = parse(pic1)
+print(picture(grid1))
+print(grid1['A1'])
+print(grid1['A9'])
+print(picture(constrain(grid1)))
+# Example when constrain is not good enough (need a search strategy)
+grid2 = parse("4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......")
+print(picture(grid2))
+print(picture(constrain(grid2)))
