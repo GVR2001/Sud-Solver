7110f4abd9d52ebce1a7df17f73b12a91729f24d
GVR2001 <gurveernirwal@gmail.com>
2025-05-12 17:13:54 +0100

    Created frames for the singular grids
    I have added code to produce single grids and now the grid looks more
    appealing, however now the code is more complex.

--- sudoku_frame.py
+++ sudoku_frame.py
@@ -2,18 +2,26 @@ import customtkinter
 
 class single_grid(customtkinter.CTkFrame):
     def __init__(self, master):
-        pass
-
-class sudokuFrame(customtkinter.CTkFrame):
-    def __init__(self, master):
         super().__init__(master)
         self.buttons = []
-        for i in range(9):
-            for j in range(9):
+        for i in range(3):
+            for j in range(3):
                 button = customtkinter.CTkButton(self, width=40, height=40, corner_radius = 0, text="1", 
                 fg_color='white',hover=True, hover_color='grey',text_color ='black')
                 button.grid(row=i,column=j)
                 self.buttons.append(button)
+        
+
+class sudokuFrame(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.grids = []
+        for i in range(3):
+            for j in range(3):
+                grid = single_grid(self)
+                grid.grid(row=i, column = j, padx = 5, pady= 5)
+                self.grids.append(grid)
+
 
         
 
@@ -23,6 +31,10 @@ class App(customtkinter.CTk):
 
         self.title("grid test")
         self.geometry("380x380")
+        #self.single_grid = single_grid(self)
+        #self.single_grid.grid(row=0, column = 0, padx = 10, pady = 10, sticky='nsew')
+        #self.single_grid2 = single_grid(self)
+        #self.single_grid2.grid(row=0, column = 1, padx = 10, pady = 10, sticky='nsew')
         self.sud_frame = sudokuFrame(self)
         self.sud_frame.grid(row=0, column=0, padx=10, pady=10,sticky="nsew")
 
