ff37ca0c9f6f64a8886d79b5ded3b4048625dc50
GVR2001 <gurveernirwal@gmail.com>
2025-05-12 16:49:03 +0100

    Experimenting with Frames for the Sudoku app
    I have been trying to develop the GUI for the sudoku app, I have made
    the grid of numbers now I will try and make it look clean.

--- sudoku_frame.py
+++ sudoku_frame.py
@@ -2,14 +2,29 @@ import customtkinter
 
 class single_grid(customtkinter.CTkFrame):
     def __init__(self, master):
-        super().__init__(master)
-
-        self.checkbox_1 = customtkinter.CTkCheckBox(self, text="checkbox 1")
-        self.checkbox_1.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
-        self.checkbox_2 = customtkinter.CTkCheckBox(self, text="checkbox 2")
-        self.checkbox_2.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
-
+        pass
 
 class sudokuFrame(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
+        self.buttons = []
+        for i in range(9):
+            for j in range(9):
+                button = customtkinter.CTkButton(self, width=40, height=40, corner_radius = 0, text="1", 
+                fg_color='white',hover=True, hover_color='grey',text_color ='black')
+                button.grid(row=i,column=j)
+                self.buttons.append(button)
+
+        
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("grid test")
+        self.geometry("380x380")
+        self.sud_frame = sudokuFrame(self)
+        self.sud_frame.grid(row=0, column=0, padx=10, pady=10,sticky="nsew")
+
+app = App()
+app.mainloop()
